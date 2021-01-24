c3476d6cf967530547fb312c16f94826e7476c83
Aleksandra <a0993715642@gmail.com>
2021-01-24 20:51:18 +0200

    add ProfileController

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\User;
+
+class ProfileController extends Controller
+{
+    /**
+     * show profile page
+     *
+     * @param Type $var Description
+     * @return type
+     * @throws conditon
+     **/
+    public function index()
+    {
+        return view('profile');
+    }
+
+    /**
+     * show profile page
+     *
+     * @param Type $var Description
+     * @return type
+     * @throws conditon
+     **/
+    public function logout()
+    {
+        Auth::logout();
+        return view('auth.login');
+    }
+}
