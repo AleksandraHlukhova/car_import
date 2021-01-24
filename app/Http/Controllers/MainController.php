c43e5cf46505e3c89ccffaf38160b0664d864d29
Aleksandra <a0993715642@gmail.com>
2021-01-24 20:50:19 +0200

    add method

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,6 +7,17 @@ use App\Product;
 
 class MainController extends Controller
 {
+
+    // /**
+    //  * Create a new controller instance.
+    //  *
+    //  * @return void
+    //  */
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+    
     /**
      * show home page
      *
