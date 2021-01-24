5d0dc5558a82c1a503f71caf4b92103cd91d5cf6
Aleksandra <a0993715642@gmail.com>
2021-01-24 20:54:20 +0200

    edit route

--- resources/views/partials/product.blade.php
+++ resources/views/partials/product.blade.php
@@ -9,7 +9,7 @@
             <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                     <a href="{{ route('product', $product->id) }}" type="button" class="btn btn-sm btn-outline-secondary">View</a>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Add to bookmarks</button>
+                    <a href="{{ route('bookmark.add', $product->id) }}" type="button" class="btn btn-sm btn-outline-secondary">Add to bookmarks</a>
                 </div>
                 <small class="text-muted">{{$product->created_at}}</small>
             </div>
