581080a529c759c94f59a177327e79e00e0f5e4c
AleksandraHlukhova <a0993715642@gmail.com>
2021-02-12 16:30:41 +0200

    config

--- config/car_import.php
+++ config/car_import.php
@@ -3,4 +3,19 @@
 return [
 
     'seeder_qty' => 10,
+    'order_status' => [
+        'paid', 'unpaid'
+    ],
+    'engine_type' => [
+        'diezel', 'petrol', 'gas'
+    ],
+    'transmission' => [
+        'mechanical', 'automatic', 'semi-automatic'
+    ],
+    'proposition_status' => [
+        'agreed', 'pending', 'rejected'
+    ],
+    
+
+
 ];
