1e5f03516a1f69b6de95500b4e790dff9e0b0ccd
AleksandraHlukhova <a0993715642@gmail.com>
2021-02-16 19:13:16 +0200

    add relationship

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
+use App\Order;
 
 class AdminController extends Controller
 {
@@ -31,5 +32,19 @@ class AdminController extends Controller
     {
         $customers = User::all();
         return view('admin.customers', ['customers' => $customers]);
+    }
+
+    /**
+     * show orders and their status
+     *
+     * @param Type $var Description
+     * @return type
+     * @throws conditon
+     **/
+    public function orders()
+    {
+        $orders = Order::all();
+        // dd($orders);
+        return view('admin.orders', ['orders' => $orders]);
     }
 }
