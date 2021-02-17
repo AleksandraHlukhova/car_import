eac4672f2ba08730167d5bd997f3cb99eba2f67b
AleksandraHlukhova <a0993715642@gmail.com>
2021-02-17 16:27:46 +0200

    create orders method

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
-use App\Order;
 
 class AdminController extends Controller
 {
@@ -34,17 +33,4 @@ class AdminController extends Controller
         return view('admin.customers', ['customers' => $customers]);
     }
 
-    /**
-     * show orders and their status
-     *
-     * @param Type $var Description
-     * @return type
-     * @throws conditon
-     **/
-    public function orders()
-    {
-        $orders = Order::all();
-        // dd($orders);
-        return view('admin.orders', ['orders' => $orders]);
-    }
 }
