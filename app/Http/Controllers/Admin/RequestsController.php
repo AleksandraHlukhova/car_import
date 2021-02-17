5a431be6817011e452a7e5e10a5443fda79f2311
AleksandraHlukhova <a0993715642@gmail.com>
2021-02-17 16:28:09 +0200

    create RequestsController

---
+++ app/Http/Controllers/Admin/RequestsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Request as Req;
+
+class RequestsController extends Controller
+{
+    /**
+     * show requests
+     *
+     * @param Type $var Description
+     * @return type
+     * @throws conditon
+     **/
+    public function show()
+    {
+        $requests = Req::get();
+        return view('admin.requests', ['requests' => $requests]);
+    }
+}
