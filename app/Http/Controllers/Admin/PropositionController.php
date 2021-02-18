e6961f07c11ee421de5926a4032e057a0d0911a2
AleksandraHlukhova <a0993715642@gmail.com>
2021-02-18 16:59:37 +0200

    change nethod show

--- app/Http/Controllers/Admin/PropositionController.php
+++ app/Http/Controllers/Admin/PropositionController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Proposition;
 use App\Product;
+use App\User;
 use App\Request as Req;
 use Illuminate\Support\Facades\Auth;
 
@@ -21,17 +22,15 @@ class PropositionController extends Controller
     public function show($id)
     {
         $propositions = Proposition::where('user_id', $id)->get();
-        $products = [];
-        foreach($propositions as $proposition)
-        {
-            $products[] = $proposition->product;
-        }
-        // dd($products);
+        $user = User::find($id);
 
-        return view('admin.products', ['products' => $products]);
+        return view('admin.proposition', [
+            'propositions' => $propositions,
+            'user' => $user
+        ]);
     }
 
-        /**
+    /**
      * propositions selct
      *
      * @param Type $var Description
@@ -61,15 +60,40 @@ class PropositionController extends Controller
         $request = Req::find($id);
         $request->status = 1;
         $request->save();
-        foreach($propositions_id as $id)
+        foreach($propositions_id as $prop_id)
         {
             Proposition::create([
                 'user_id' => $request->user_id,
-                'product_id' => $id,
-                'status' => 'pending'
+                'product_id' => $prop_id,
+                'request_id' => $id
             ]);
         }
         
         return redirect()->route('admin.requests.show');
+    }
+
+    /**
+     * propositions selct
+     *
+     * @param Type $var Description
+     * @return type
+     * @throws conditon
+     **/
+    public function delete($id)
+    {
+        $proposition = Proposition::find($id);
+        Proposition::find($id)->delete();
+
+        $requestHasPropositions = Proposition::where('request_id', $proposition->request_id)->get();
+
+        // dd(count($requestHasPropositions) === 0);
+        
+        if(count($requestHasPropositions) === 0)
+        {
+            $request = Req::find($proposition->request_id);
+            $request->status = 0;
+            $request->save();
+        }
+        return back();
     }
 }
