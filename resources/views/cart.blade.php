e924f922eae1b9e6a328952a5c8458f1729a4317
AleksandraHlukhova <a0993715642@gmail.com>
2021-02-24 16:31:08 +0200

    cart view

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -2,27 +2,47 @@
 @section('title', 'Cart')
 
 @section('content')
-
-@if(count($products) === 0)
-<p>Cart is empty</p>
-@else
-<table class="table">
-    <thead>
+<div class="container">
+    @if(count($order->products) === 0)
+    <p>Cart is empty</p>
+    @else
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Brand</th>
+                <th scope="col">Model</th>
+                <th scope="col">Year</th>
+                <th scope="col">Price</th>
+            </tr>
+        </thead>
+        @php
+        $i = 1;
+        $sum = 0;
+        @endphp
+        @foreach($order->products as $product)
+        <tbody>
+            <tr>
+                <th scope="row">{{$i}}</th>
+                <td>{{$product->brand}}</td>
+                <td>{{$product->model}}</td>
+                <td>{{$product->year}}</td>
+                <td>{{$product->price}} USD</td>
+            </tr>
+        </tbody>
+        @php
+        $i++;
+        $sum += $product->price;
+        @endphp
+        @endforeach
         <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="row">Total:</th>
+            <td colspan="2"></td>
+            <td></td>
+            <th>{{$sum}} USD</th>
         </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-        </tr>
-    </tbody>
-</table>
-@endif
+    </table>
+    @endif
+    <input type="submit" value="Buy">
+</div>
 @endsection
